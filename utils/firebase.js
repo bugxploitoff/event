4cb73f4ee67deca053b9797cda41333ae2835807
Sumon Nath <snath2973@gmail.com>
2023-09-22 02:06:33 +0530

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -5,21 +5,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-
   apiKey: "AIzaSyCzQP3jcw-FLgCBm9iMgpDF7j0voUUpqjY",
-
   authDomain: "events-c2e88.firebaseapp.com",
-
   projectId: "events-c2e88",
-
   storageBucket: "events-c2e88.appspot.com",
-
   messagingSenderId: "638716242047",
-
-  appId: "1:638716242047:web:c693d1f2611fe4deb9374a",
-
-  measurementId: "G-GQQW6W5LWK"
-
+  appId: "1:638716242047:web:0f5b412bff895234b9374a",
+  measurementId: "G-TY0M13ECGZ"
 };
 
 // Initialize Firebase
